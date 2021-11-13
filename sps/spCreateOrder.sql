eadbe8e8b028f48fc5b1c77a78d844131dc6814c
Matt Crabtree <m.crabtree@zohomail.eu>
2021-11-13 14:47:19 +0000

    formatting

--- sps/spCreateOrder.sql
+++ sps/spCreateOrder.sql
@@ -7,24 +7,50 @@ begin;
 end;
 
 GO
+
 CREATE procedure dbo.spCreateOrder
 (
     @customerId int,
     @addressId int, 
-    @pickupSlot int, 
-    @deliverySlot int, 
-    @creationDate smalldatetime, 
-    @modifiedDate smalldatetime,
+    @pickupSlotId int = null,
+    @deliverySlotId int = null,
+    @creationDate smalldatetime = default, 
+    @modifiedDate smalldatetime = default,
     @grandTotal float, 
     @itemCount int, 
     @pointsEarned int, 
-    @fulfilmentInstructions varchar(100)
+    @fulfilmentInstructions varchar(100) = null
 )
 as
 BEGIN;
-declare @newId int
+declare @newId int,
+@todayDate as date
+set @todayDate = getDate()
 
-
-
+insert into
+    dbo.tblOrder(
+        customerId,
+        addressId,
+        pickupSlotId,
+        deliverySlotId,
+        creationDate,
+        modifiedDate,
+        grandTotal,
+        itemCount,
+        pointsEarned,
+        fulfilmentInstructions
+        )
+values(
+    @customerId,
+    @addressId,
+    @pickupSlotId,
+    @deliverySlotId,
+    @todayDate,
+    @todayDate,
+    @grandTotal,
+    @itemCount,
+    @pointsEarned,
+    @fulfilmentInstructions
+    )
 
 END;
