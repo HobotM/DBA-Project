5f2a2370b2788a936f029e831477914cb6efc4ba
Matt Crabtree <m.crabtree@zohomail.eu>
2021-11-13 14:35:29 +0000

    added product sp

--- sps/spCreateProduct.sql
+++ sps/spCreateProduct.sql
@@ -2,7 +2,19 @@ use AMMTescoProject
 GO
 create procedure dbo.spCreateProduct
 (
-    
+    @SKU varchar(20),
+    @departmentId varchar(20),
+    @price float,
+    @brand varchar(30),
+    @description varchar(100) = null,
+    @name varchar(30),
+    @tagline varchar(50) = null,
+    @discounted int = 0,
+    @weight float
 )
+
 as
 begin;
+insert into tblProduct(departmentId, SKU, price, brand, description, name, tagline, discounted, weight)
+    values(@departmentId, @SKU, @price, @brand, @description, @name, @tagline, @discounted, @weight)
+end;
