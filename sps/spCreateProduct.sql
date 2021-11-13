45b4a4a7580b7fc5a336540ae64fd1a9c206c1e8
Matt Crabtree <m.crabtree@zohomail.eu>
2021-11-13 15:08:23 +0000

    formatting

--- sps/spCreateProduct.sql
+++ sps/spCreateProduct.sql
@@ -1,20 +1,41 @@
 use AMMTescoProject
 GO
-create procedure dbo.spCreateProduct
-(
-    @SKU varchar(20),
-    @departmentId varchar(20),
-    @price float,
-    @brand varchar(30),
-    @description varchar(100) = null,
-    @name varchar(30),
-    @tagline varchar(50) = null,
-    @discounted int = 0,
-    @weight float
-)
+    create procedure dbo.spCreateProduct (
+        @SKU varchar(20),
+        @departmentId varchar(20),
+        @price float,
+        @brand varchar(30),
+        @description varchar(100) = null,
+        @name varchar(30),
+        @tagline varchar(50) = null,
+        @discounted int = 0,
+        @weight float
+    ) as begin;
 
-as
-begin;
-insert into tblProduct(departmentId, SKU, price, brand, description, name, tagline, discounted, weight)
-    values(@departmentId, @SKU, @price, @brand, @description, @name, @tagline, @discounted, @weight)
+insert into
+    tblProduct(
+        departmentId,
+        SKU,
+        price,
+        brand,
+        description,
+        name,
+        tagline,
+        discounted,
+        weight
+    )
+values
+(
+        @departmentId,
+        @SKU,
+        @price,
+        @brand,
+        @description,
+        @name,
+        @tagline,
+        @discounted,
+        @weight
+    )
 end;
+
+go
